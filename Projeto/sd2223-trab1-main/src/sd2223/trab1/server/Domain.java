ffbb7d611ae9808d852f925dcad0896e28c8149f
miguelvalido02 <m.valido@campus.fct.unl.pt>
2023-03-28 12:08:37 +0100

    domain

---
+++ Projeto/sd2223-trab1-main/src/sd2223/trab1/server/Domain.java
@@ -0,0 +1,13 @@
+package sd2223.trab1.server;
+
+public class Domain {
+    private static String domain;
+
+    public static void setDomain(String domain) {
+        Domain.domain = domain;
+    }
+
+    public static String getDomain() {
+        return domain;
+    }
+}
