d73fa51391fa76e05f267461a88f260364280399
ArturHoral <a.horal@campus.fct.unl.pt>
2023-04-04 10:58:59 +0100

    Adicao de soap, modificacao de todos os ficheiros

--- Projeto/sd2223-trab1-main/src/sd2223/trab1/clients/rest/RestUsersClient.java
+++ Projeto/sd2223-trab1-main/src/sd2223/trab1/clients/rest/RestUsersClient.java
@@ -1,9 +1,11 @@
-package sd2223.trab1.clients;
+package sd2223.trab1.clients.rest;
 
 import java.net.URI;
 import java.util.List;
 
 import sd2223.trab1.api.User;
+import sd2223.trab1.api.java.Result;
+import sd2223.trab1.api.java.Users;
 import jakarta.ws.rs.client.Entity;
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.core.MediaType;
@@ -12,7 +14,7 @@ import jakarta.ws.rs.core.GenericType;
 import sd2223.trab1.api.rest.UsersService;
 import jakarta.ws.rs.core.Response.Status;
 
-public class RestUsersClient extends RestClient implements UsersService {
+public class RestUsersClient extends RestClient implements Users {
 
     final WebTarget target;
 
@@ -21,7 +23,7 @@ public class RestUsersClient extends RestClient implements UsersService {
         target = client.target(serverURI).path(UsersService.PATH);
     }
 
-    private String clt_createUser(User user) {
+    private Result<String> clt_createUser(User user) {
 
         Response r = target.request()
                 .accept(MediaType.APPLICATION_JSON)
@@ -29,14 +31,14 @@ public class RestUsersClient extends RestClient implements UsersService {
 
         if (r.getStatus() == Status.OK.getStatusCode() && r.hasEntity()) {
             System.out.println("User criado " + r.getStatus());
-            return r.readEntity(String.class);
+            return super.toJavaResult(r, String.class);
         } else
             System.out.println("Error, HTTP error status: " + r.getStatus());
 
         return null;
     }
 
-    private User clt_getUser(String name, String pwd) {
+    private Result<User> clt_getUser(String name, String pwd) {
         User u = null;
         Response r = target.path(name)
                 .queryParam(UsersService.PWD, pwd).request()
@@ -44,12 +46,11 @@ public class RestUsersClient extends RestClient implements UsersService {
                 .get();
         if (r.getStatus() == Status.OK.getStatusCode() && r.hasEntity()) {
             System.out.println("Success:");
-            u = r.readEntity(User.class);
+            u = r.readEntity(User.class); // return aqui?
             System.out.println("User : " + u);
         } else
             System.out.println("Error, HTTP error status: " + r.getStatus());
-
-        return u;
+        return super.toJavaResult(r, User.class);
     }
 
     private User clt_updateUser(String name, String pwd, User user) {
@@ -105,12 +106,12 @@ public class RestUsersClient extends RestClient implements UsersService {
     }
 
     @Override
-    public String createUser(User user) {
+    public Result<String> createUser(User user) {
         return super.reTry(() -> clt_createUser(user));
     }
 
     @Override
-    public User getUser(String name, String pwd) {
+    public Result<User> getUser(String name, String pwd) {
         return super.reTry(() -> clt_getUser(name, pwd));
     }
 
@@ -132,5 +133,11 @@ public class RestUsersClient extends RestClient implements UsersService {
     @Override
     public User userExists(String name) {
         return null;
+    }
+
+    @Override
+    public Result<Void> verifyPassword(String name, String pwd) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'verifyPassword'");
     }
 }
