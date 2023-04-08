08ffdf24e8b4d31140289387b7485afa04674064
miguelvalido02 <m.valido@campus.fct.unl.pt>
2023-04-08 13:30:00 +0100

    suspect, qual o timeout e max_retries adequado?

--- Projeto/sd2223-trab1-main/src/sd2223/trab1/clients/rest/RestClient.java
+++ Projeto/sd2223-trab1-main/src/sd2223/trab1/clients/rest/RestClient.java
@@ -26,7 +26,7 @@ public class RestClient {
     protected static final int READ_TIMEOUT = 5000;
     protected static final int CONNECT_TIMEOUT = 5000;
 
-    protected static final int RETRY_SLEEP = 500;
+    protected static final int RETRY_SLEEP = 1000;
     protected static final int MAX_RETRIES = 10;
 
     final URI serverURI;
